2532a257c82ae728d9b0e5eb159274abd35cb8ec
Christopher Chamaletsos <chrishham@yahoo.gr>
2019-03-20 14:09:22 +0200

    updated index.html

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,13 @@
             a(href="#" @click="open('https://electronjs.org/')" ) Electron 
             | app, created with ❤️ by 
             | Christopher Chamaletsos .
-            //- img(src="@/assets/GitHub-Mark-32px.png")
+            br
+            a(href="#" @click="open('https://github.com/chrishham/BypassCors/blob/master/LICENSE')") MIT
+            |  license.
+            span  Hosted at 
+              a(href="#" @click="open('https://github.com/chrishham/BypassCors')")
+                img.githubIcon(src="@/assets/GitHub-Mark-32px.png" alt="Github Icon" class="githubIcon")
+              |  .
   </template>
   
 <script>
@@ -27,4 +33,7 @@ export default {
 </script>
 
 <style scoped>
+.githubIcon {
+  width: 25px;
+}
 </style>
